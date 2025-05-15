fc86daf871272f0e07b3b1eb7efd15cb64d246b5
Fenix125 <migelspainn@gmail.com>
2025-05-16 00:11:43 +0300

    commit N2, testing email senter

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const crypto = require("crypto");
 const { fetchCurrentWeather } = require("./services/weather");
 const usersRouter = express.Router();
 const swaggerUi = require("swagger-ui-express");
@@ -8,11 +9,11 @@ const app = express();
 const port = 3000;
 const swaggerDocument = YAML.load("./swagger.yaml");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api", usersRouter);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 let ALL_USERS = [];
 
@@ -29,8 +30,31 @@ usersRouter.get("/weather", async (req, res) => {
     }
 });
 
+function makeToken(bytes = 16) {
+    return crypto.randomBytes(bytes).toString("hex");
+}
+
 usersRouter.post("/subscribe", (req, res) => {
-    res.json({ message: "Makes subscribtion" });
+    const { email, city, frequency } = req.body;
+    if (
+        !email ||
+        !city ||
+        !frequency ||
+        !["hourly", "daily"].includes(frequency)
+    ) {
+        return res.status(400).json({ error: "Invalid input" });
+    }
+    const exists = ALL_USERS.some(
+        (u) => u.email === email && u.city === city && u.frequency === frequency
+    );
+    if (exists) {
+        return res.status(409).json({
+            error: "Email already subscribed",
+        });
+    }
+
+    const token = makeToken();
+    ALL_USERS.push({ email, city, frequency, confirmed: false, token });
 });
 
 usersRouter.get("/confirm/:token", (req, res) => {
