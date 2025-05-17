572be7058f58f4500a47e3ba42e12ce159366ee0
Fenix125 <migelspainn@gmail.com>
2025-05-17 13:33:05 +0300

    commit N3, send emails Implemnted email sending every hour/day, needs fixes of repeatable functions and general structure of code

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const crypto = require("crypto");
+const cron = require("node-cron");
 const { fetchCurrentWeather } = require("./services/weather");
+const {
+    sendConfirmationEmail,
+    sendUpdates,
+} = require("./services/email_sender");
 const usersRouter = express.Router();
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
@@ -9,11 +14,11 @@ const app = express();
 const port = 3000;
 const swaggerDocument = YAML.load("./swagger.yaml");
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use("/api", usersRouter);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use("/api", usersRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 let ALL_USERS = [];
 
@@ -24,7 +29,7 @@ usersRouter.get("/weather", async (req, res) => {
     }
     try {
         const weather = await fetchCurrentWeather(city);
-        res.json(weather);
+        res.status(200).json(weather);
     } catch (error) {
         res.status(404).json({ error: "City not found" });
     }
@@ -32,6 +37,9 @@ usersRouter.get("/weather", async (req, res) => {
 
 function makeToken(bytes = 16) {
     return crypto.randomBytes(bytes).toString("hex");
+}
+function tokenizeEmail(email) {
+    return Buffer.from(email).toString("base64url");
 }
 
 usersRouter.post("/subscribe", (req, res) => {
@@ -52,17 +60,83 @@ usersRouter.post("/subscribe", (req, res) => {
             error: "Email already subscribed",
         });
     }
-
-    const token = makeToken();
-    ALL_USERS.push({ email, city, frequency, confirmed: false, token });
+    const token_email = tokenizeEmail(email);
+    const random_token = makeToken();
+    const token = `${token_email}.${random_token}`;
+    ALL_USERS.push({
+        email,
+        city,
+        frequency,
+        confirmed: false,
+        token: random_token,
+    });
+    sendConfirmationEmail(token, email);
+    res.status(200).json({
+        message: "Subscription successful. Confirmation email sent.",
+    });
 });
 
 usersRouter.get("/confirm/:token", (req, res) => {
-    res.json({ message: "Confirms subscribtion" });
+    const { token } = req.params;
+    const parts = token.split(".");
+    if (parts.length !== 2) {
+        return res.status(400).json({ error: "Invalid token" });
+    }
+    const [emailPart, randomPart] = parts;
+
+    let email = Buffer.from(emailPart, "base64url").toString("utf8");
+    const user = ALL_USERS.find((u) => u.email === email);
+    if (!user) {
+        return res.status(404).json({ error: "Token not found" });
+    }
+    if (user.token !== randomPart) {
+        return res.status(400).json({ error: "Invalid token" });
+    }
+    user.confirmed = true;
+    res.json({ message: "Subscription confirmed successfully" });
 });
 
 usersRouter.get("/unsubscribe/:token", (req, res) => {
-    res.json({ message: "Unsubscribes" });
+    const { token } = req.params;
+    const parts = token.split(".");
+    if (parts.length !== 2) {
+        return res.status(400).json({ error: "Invalid token" });
+    }
+    const [emailPart, randomPart] = parts;
+    let email = Buffer.from(emailPart, "base64url").toString("utf8");
+    const user = ALL_USERS.find((u) => u.email === email);
+    if (!user) {
+        return res.status(404).json({ error: "Token not found" });
+    }
+    if (user.token !== randomPart) {
+        return res.status(400).json({ error: "Invalid token" });
+    }
+    ALL_USERS = ALL_USERS.filter((u) => u.email !== email);
+    res.json({ message: "Unsubscribed successfully" });
+});
+
+usersRouter.get("/users", (req, res) => {
+    res.json(ALL_USERS);
 });
 
 app.listen(port);
+
+async function sendWeatherUpdates(frequency) {
+    const users = ALL_USERS.filter(
+        (u) => u.confirmed === true && u.frequency === frequency
+    );
+    for (const u of users) {
+        const weather = await fetchCurrentWeather(u.city);
+        const token_email = tokenizeEmail(u.email);
+        const token = `${token_email}.${u.token}`;
+        sendUpdates(token, u.email, weather);
+    }
+}
+
+cron.schedule("0 * * * *", () => {
+    sendWeatherUpdates("hourly");
+});
+
+cron.schedule("0 8 * * *", () => {
+    sendWeatherUpdates("daily");
+});
---
+++ services/email_sender.js
@@ -0,0 +1,63 @@
+require("dotenv").config();
+const fs = require("fs");
+const nodemailer = require("nodemailer");
+
+const email = process.env.GMAIL;
+const email_pass = process.env.GMAIL_PASS;
+
+const htmlTemplate = fs.readFileSync(
+    "email_template/email_confirm.html",
+    "utf8"
+);
+const htmlTemplateUpdate = fs.readFileSync(
+    "email_template/weather_updates.html",
+    "utf8"
+);
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: `${email}`,
+        pass: `${email_pass}`,
+    },
+});
+
+function buildConfirmationEmail(token) {
+    const confirmUrl = `http://localhost:3000/api/confirm/${token}`;
+    return htmlTemplate
+        .replace("{{TOKEN}}", token)
+        .replace("{{CONFIRM_LINK}}", confirmUrl);
+}
+
+function buildUpdateEmail(token, weather) {
+    const unsubscribeLink = `http://localhost:3000/api/unsubscribe/${token}`;
+    return htmlTemplateUpdate
+        .replace("{{TEMPERATURE}}", weather.temperature)
+        .replace("{{HUMIDITY}}", weather.humidity)
+        .replace("{{DESCRIPION}}", weather.description)
+        .replace("{{CONFIRM_LINK}}", unsubscribeLink);
+}
+
+function sendConfirmationEmail(token, user_email) {
+    const html = buildConfirmationEmail(token);
+    const mailOptions = {
+        from: `${email}`,
+        to: `${user_email}`,
+        subject: "Please confirm your WeatherAPI subscription",
+        html: html,
+    };
+    transporter.sendMail(mailOptions);
+}
+
+function sendUpdates(token, user_email, weather) {
+    const html = buildUpdateEmail(token, weather);
+    const mailOptions = {
+        from: `${email}`,
+        to: `${user_email}`,
+        subject: "WeatherAPI updates",
+        html: html,
+    };
+    transporter.sendMail(mailOptions);
+}
+
+module.exports = { sendConfirmationEmail, sendUpdates };
